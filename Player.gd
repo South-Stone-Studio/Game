3b58e99df39fc6d755c928c837314f4e09674eed
DIDUS20 <didusogiel@gmail.com>
2024-05-22 17:00:36 +0200

    add player node
    lolololololol

---
+++ Player.gd
@@ -0,0 +1,14 @@
+class_name Player_movement
+
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(	"HELLO WORLD") # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
