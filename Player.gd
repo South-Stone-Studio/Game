fc0b65c2bdc5438293ae7929d489c087e92fa839
DIDUS20 <didusogiel@gmail.com>
2024-05-24 07:39:59 +0200

    Adding basic player and movement
    added camera , basic floor and look at mouse

--- Player.gd
+++ Player.gd
@@ -1,13 +1,31 @@
-class_name Player_movement
-
-extends Node3D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print(	"HELLO WORLD") # Replace with function body.
+extends CharacterBody3D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+const SPEED: float = 5.0
+const JUMP_VELOCITY: float = 4.5
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: Variant = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
