9b80769fec31233337aeb83d3afe0314bf86719f
DIDUS20 <didusogiel@gmail.com>
2024-05-25 23:03:55 +0200

    Player input and Camera update
    WASD , Camera follow player and offset

--- scripts/player/Player_movement.gd
+++ scripts/player/Player_movement.gd
@@ -22,7 +22,16 @@ func _physics_process(delta):
 	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	rayOrigin = camera.project_ray_origin(mouse_position)
-	rayEnd = rayOrigin + camera.project_ray_normal(mouse_position) * 2000
+	var ray_normal:= camera.project_ray_normal(mouse_position)
+	ray_normal = ray_normal.limit_length(1)
+	rayEnd = rayOrigin + ray_normal * 2000
+	
+	# Camera operations / Camera follow cursor / Camera offset from player
+	camera.position = position
+	camera.position += ray_normal*4
+	camera.position.y = 10
+	camera.position.z += 2
+	
 	var ray_parameters: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(rayOrigin,rayEnd)
 	var intersection: Dictionary = space_state.intersect_ray(ray_parameters)
 	if not intersection.is_empty():
@@ -32,8 +41,8 @@ func _physics_process(delta):
 	player_movement(delta)
 
 func get_input():
-	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	input.y = int(Input.is_action_pressed("ui_down")) - int((Input.is_action_pressed("ui_up")))
+	input.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	input.y = int(Input.is_action_pressed("move_down")) - int((Input.is_action_pressed("move_up")))
 	return input.normalized()
 
 func player_movement(delta):
