783f67b33e05f7dc948650a26c6b530297552262
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2024-06-08 14:53:10 +0200

    rename folder names

--- scripts/weapons and items/weapon types/Blaziken.gd
+++ scripts/weapons and items/weapon types/Blaziken.gd
@@ -2,6 +2,10 @@ class_name Blaziken
 
 extends BaseWeapon
 @export var atack_speed: float
-
+# SMG Vibes
+# Quick fire rate
+# Small mag
+# constant reload
+# low damage
 func primary():
-	print("shoot")
+	pass
