1bf044d4ef4e4492b92161aff67c42ec2465a364
DIDUS20 <didusogiel@gmail.com>
2024-06-24 22:59:16 +0200

    add attack to sceleton
    update of sceleto attack and hold position style

--- scripts/enemies/skeleton/Skeleton.gd
+++ scripts/enemies/skeleton/Skeleton.gd
@@ -2,7 +2,7 @@ class_name EnemyMovement
 
 extends Enemy
 
-enum s_types {mele, range, magic}
+enum s_types {mele, range}
 @export var sceleton_type : s_types
 
 @export_category("vision")
@@ -12,11 +12,10 @@ enum s_types {mele, range, magic}
 @export var vision_raycast : RayCast3D
 var saved_player_position : Vector3
 
-@export_category("patrol options")
-enum patrol {hold_position, random_patrol, prepared_patrol}
+@export_category("patrol")
+enum patrol {hold_position, prepared_patrol}
 @export var patrol_mode : patrol
 @export var patrol_points : Array[Marker3D]
-@export var hold_position_points : Array[Vector3]
 var hunt : bool = false
 var alarmed : bool = false
 
@@ -24,16 +23,30 @@ var alarmed : bool = false
 @export var radius : int
 var d : float = 0.0
 
+@export_category("mele type")
+@export var sword : Area3D
+@export var demage : int
+@export var attack_speed : float
+var attack_timer : float = 0.0
+
+@export_category("mele type")
+
 func _ready() -> void:
 	super._ready()
-	saved_player_position = self.position
+	saved_player_position = position
 	player = Global.player
+	set_type_of_sceleton()
 
 func _process(delta : float) -> void:
 	super._process(delta)
+	velocity = Vector3.ZERO
 	# hunting and alarm
 	if hunt:
 		move_to_pos(player.position)
+		attack_timer += attack_speed * delta
+		if attack_timer >= 100:
+			attack_timer = 0.0
+			attack(delta, demage)
 	elif !hunt and alarmed:
 		if position.distance_to(saved_player_position) >= 1:
 			move_to_pos(saved_player_position)
@@ -44,20 +57,21 @@ func _process(delta : float) -> void:
 
 func move_to_pos(pos : Vector3) -> void:
 	velocity = Vector3.ZERO
+	# use nav agent to find path to target
 	nav_agent.set_target_position(pos)
 	var next_nav_point : Vector3 = nav_agent.get_next_path_position()
 	velocity = (next_nav_point - global_transform.origin).normalized() * speed
 	move_and_slide()
 
 func _on_vision_timer_timeout() -> void:
+	hunt = false
 	var overlaps = vision.get_overlapping_bodies()
 	if overlaps.size() > 0:
-		hunt = false
 		for overlap in overlaps:
 			if overlap.name == "Player":
 				alarmed = true
 				vision_raycast.force_raycast_update()
-				self.look_at(Vector3(player.position.x, 0.5, player.position.z), Vector3(0, 1, 0))
+				look_at(Vector3(player.position.x, 0.5, player.position.z), Vector3(0, 1, 0))
 				if vision_raycast.is_colliding():
 					var collider : Object = vision_raycast.get_collider()
 					if collider.name == "Player":
@@ -75,4 +89,18 @@ func do_patrol(delta : float) -> void:
 	elif patrol_mode == patrol.prepared_patrol:
 		# visit points seted at the start
 		pass
-	
+
+func attack(delta : float, demage : int) -> void: 
+	if sceleton_type == s_types.mele:	#mele
+		print("mele")
+		sword.rotate_y(speed * delta)
+		var overlaps = vision.get_overlapping_bodies()
+		if overlaps.size() > 0:
+			for overlap in overlaps:
+				if overlap.name == "Player":
+					print("demage to player")
+	elif sceleton_type == s_types.range:	#ranged
+		print("range")
+
+func set_type_of_sceleton() -> void:
+	pass
