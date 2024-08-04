94f7c6d721aaf25b9ac1b293e1799198224d6afd
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2024-08-04 19:44:46 +0200

    correct how slime moves
    now he cant change velocity mid jump only when touching floor

--- scripts/movement component/SlimeMovement.gd
+++ scripts/movement component/SlimeMovement.gd
@@ -7,33 +7,31 @@ var is_jump: bool
 
 func move(delta:float, target_position: Vector3) -> void:
 	var direction = (target_position - global_position).normalized()
-	direction.x = snapped(direction.x, 0.001)
-	direction.z = snapped(direction.z, 0.001)
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
-	print(velocity)
-	check_floor(delta)
+	if check_floor(delta):
+		direction.x = snapped(direction.x, 0.001)
+		direction.z = snapped(direction.z, 0.001)
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	move_and_slide()
 	
 func stand(delta: float) -> void:
-	velocity.x = 0
-	velocity.z = 0
-	check_floor(delta)
+	if check_floor(delta):
+		velocity.x = 0
+		velocity.z = 0
 	move_and_slide()
 	
-func check_floor(delta: float):
+func check_floor(delta: float) -> bool:
 	if not is_on_floor():
 		if !is_jump:
 			is_jump = true
 			on_jump.emit()
 		apply_gravity(delta)
-	else:
-		
-		if is_jump:
-			is_jump = false
-			on_land.emit()
-			jump()
-			
+		return false
+	if is_jump:
+		is_jump = false
+		on_land.emit()
+		jump()
+	return true
 func apply_gravity(delta: float) -> void:
 	velocity.y -= gravity * delta
 
